cbd3320179e9deb73431200742eefbd6bd4714c1
kang yeon joo <rkdduswndi@naver.com>
2023-10-15 23:19:21 +0900

    feat: 제 12장) 트랜잭션 기능 추가

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -9,8 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -54,5 +56,21 @@ public class ArticleService {
         articleRepository.delete(findArticle);
         //삭제된 Article 객체는 findArticle에 여전히 메모리에 저장되어 있으므로 이를 반환
         return findArticle;
+    }
+
+    @Transactional
+    public List<Article>  createArticles(List<ArticleDto> dtos){
+        //1. 스트림을 이용하여 dto 묶음을 엔티티 묶음으로 변환하기
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+        //2. 스트림을 이용하여 엔티티 묶음을 db에 저장하기
+        articleList.stream()
+                .forEach(article->articleRepository.save(article));
+        //3. 강제 예외 발생시키기
+        articleRepository.findById(-1L)
+                .orElseThrow(()->new IllegalArgumentException("결제 실패!"));
+        //4. 결과 값 반환하기
+        return articleList;
     }
 }
