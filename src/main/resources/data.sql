7f4bc7bda054cd284a8fc03223ab1223f7736859
kang yeon joo <rkdduswndi@naver.com>
2023-10-29 01:21:03 +0900

    feat: 제 14장) Comment 엔티티, 레포지토리 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,23 @@
 INSERT INTO article(title, content) VALUES('title 1', 'content 1');
 INSERT INTO article(title, content) VALUES('title 2', 'content 2');
 INSERT INTO article(title, content) VALUES('title 3', 'content 3');
+
+-- article 테이블에 데이터 추가
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 고');
+INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?', '댓글 고고');
+INSERT INTO article(title, content) VALUES('당신의 취미는?', '댓글 고고고');
+
+-- 4번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Park', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Kim', '아이 엠 샘');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Choi', '쇼생크 탈출');
+
+-- 5번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Park', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Kim', '샤브샤브');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Choi', '초밥');
+
+-- 6번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Park', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Kim', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Choi', '독서');
